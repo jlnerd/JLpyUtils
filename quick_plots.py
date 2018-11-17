2fae02b01f3813857c8e20323487318670f526b0
John Leonard <johnleonard@Johns-MacBook-Pro-2.local>
2018-11-17 14:04:43 -0800

    update

---
+++ quick_plots.py
@@ -0,0 +1,46 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#To call in code, add the following command
+#import sys
+#sys.path.insert(0, '.\test')
+
+#class quick_plots():
+   
+def __init__(self):
+    print("quick plots by John Leonard")
+
+#normal plot with blue pts
+def plot(df,x_label,y_label):
+    plt.plot(df[x_label],df[y_label],'ob')
+    plt.xlabel(x_label,fontsize = 16)
+    plt.ylabel(y_label,fontsize = 16)
+    plt.grid(which='major',color = 'dimgray')
+    plt.grid(which='minor',color='lightgray')
+    plt.tick_params(axis='both',labelsize = 14)
+
+#log log plot
+def loglog_plot(df,x_label,y_label):
+    plot(df,x_label,y_label)
+    plt.xscale("log")
+    plt.yscale("log")
+
+#Plot correlation matrix
+def plot_corr(df,size=10):
+    '''Function plots a graphical correlation matrix for each pair of columns in the dataframe.
+
+    Input:
+        df: pandas DataFrame
+        size: vertical and horizontal size of the plot'''
+
+    corr = df.corr()
+    fig, ax = plt.subplots(figsize=(size, size))
+    ax.matshow(corr)
+    plt.xticks(range(len(corr.columns)), corr.columns,rotation='vertical');
+    plt.yticks(range(len(corr.columns)), corr.columns);
+    
+#Save this notebook as python version
+
